c9d646bd06e00eba4ae7ada23d5c8ea688b02abd
Geniusning <690984649@qq.com>
2022-08-04 22:35:54 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,99 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-table @selection-change="selectChange" border :data="dataList">
+      <el-table-column
+        :resizable="false"
+        min-width="30px"
+        :render-header="renderHeader"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <el-checkbox
+            v-model="scope.row.itemCheck"
+            @change="toggleCheck(scope.row)"
+            :disabled="disableCheckout(scope.row)"
+            :checked="checked(scope.row)"
+          ></el-checkbox>
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="name" show-overflow-tooltip label="名称">
+      </el-table-column>
+      <el-table-column prop="age" show-overflow-tooltip label="名称">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+const cityOptions = ['上海', '北京', '广州', '深圳']
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      checkAll: false,
+      checkedCities: ['上海', '北京'],
+      cities: cityOptions,
+      isIndeterminate: true,
+      //------------------
+      dataList: [
+        { id: 0, name: '0333', age: 12, itemCheck: false },
+        { id: 1, name: '1333', age: 12, itemCheck: false },
+        { id: 2, name: '2333', age: 12, itemCheck: false },
+        { id: 3, name: '3333', age: 12, itemCheck: false },
+        { id: 4, name: '4333', age: 12, itemCheck: false },
+        { id: 5, name: '5333', age: 12, itemCheck: false }
+      ],
+      isCheck: false,
+      indeterminate: false,
+      checkedCount: 0,
+      selection: []
+    }
+  },
+  methods: {
+    checked(row) {
+      if (row.id === 4) {
+        row.checked = true
+        return true
+      }
+    },
+    selectChange(selected) {
+      console.log(selected)
+    },
+    renderHeader(h) {
+      return h('span', [
+        h('el-checkbox', {
+          on: {
+            change: this.selectBox
+          },
+          props: {
+            value: this.isCheck,
+            indeterminate: this.indeterminate
+          }
+        })
+      ])
+    },
+    selectBox() {
+      this.isCheck = !this.isCheck
+      let list = [...this.dataList]
+      console.log('list', list)
+      for (let val of list) {
+        val.itemCheck = this.isCheck
+      }
+      this.dataList = list
+    },
+    toggleCheck(row) {
+      // 获取已勾选
+      let list = this.dataList.filter((item) => item.itemCheck)
+      this.isCheck = list.length === this.dataList.length
+      this.indeterminate = list.length > 0 && list.length < this.dataList.length
+      console.log(this.indeterminate, row)
+    },
+    disableCheckout(row) {
+      // console.log('row', row)
+      return row.id === 4
+    }
+  },
+  watch: {}
 }
 </script>
 
